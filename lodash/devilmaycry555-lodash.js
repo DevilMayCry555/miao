b56627e3280ae5026f9bda75481aaa7efdf4684a
HeZi <ly530812118@gmail.com>
2022-02-14 13:36:14 +0800

    123123

--- lodash/devilmaycry555-lodash.js
+++ lodash/devilmaycry555-lodash.js
@@ -6,7 +6,7 @@ var devilmaycry555 = {
     var i = 0
     while (i < l) {
       var a = []
-      for (j = i; i + size < l ? j < i + size : j < l; j++){
+      for (var j = i; i + size < l ? j < i + size : j < l; j++){
         a.push(array[j])
       }
       newone.push(a)
@@ -30,7 +30,7 @@ var devilmaycry555 = {
     method = this.iteratee(method)
     return array.filter(function (n) {
       for (var val of value) {
-        if (mothod(val) == mothod(n)) return false;
+        if (method(val) == method(n)) return false;
       }
       return true
     })
@@ -48,7 +48,7 @@ var devilmaycry555 = {
     var l = array.length
     if (n >= l) return [];
     var a = []
-    for (i = n; i < l; i++){
+    for (var i = n; i < l; i++){
       a.push(array[i])
     }
     return a
@@ -58,7 +58,7 @@ var devilmaycry555 = {
     var l = array.length
     if (n >= l) return [];
     var a = array
-    for (i = 0; i < n; i++){
+    for (var i = 0; i < n; i++){
       a.pop()
     }
     return a
@@ -89,7 +89,7 @@ var devilmaycry555 = {
   },
   //O数组从start到end都用value表示
   fill: function (array, value, start = 0, end = array.length) {
-    for (i = start; i < end; i++){
+    for (var i = start; i < end; i++){
       array[i] = value
     }
     return array
@@ -106,7 +106,7 @@ var devilmaycry555 = {
     }
     if (this.typeSee(value) == this.typeSee(other)) {
       if (this.typeSee(value) == 'Array' && value.length == other.length) {
-        for (i = 0; i < value.length; i++){
+        for (var i = 0; i < value.length; i++){
           if (this.isEqual(value[i], other[i]) == false) return false;
         }
         return true
@@ -133,7 +133,7 @@ var devilmaycry555 = {
     }
     if (this.typeSee(object) == this.typeSee(source)) {
       if (this.typeSee(object) == 'Array' && object.length >= source.length) {
-        for (i = 0; i < source.length; i++){
+        for (var i = 0; i < source.length; i++){
           if (this.isMatch(object[i], source[i]) == false) return false;
         }
         return true
@@ -168,7 +168,7 @@ var devilmaycry555 = {
       return this.identity(func)
     }
   },
-  //返回一个接参函数 与参数对象做深度对比，若参数对象与source的属性值相同，则返回true
+  //O返回一个接参函数 与参数对象做深度对比，若参数对象与source的属性值相同，则返回true
   matches: function (source) {
     return function (element) {
       if (devilmaycry555.isMatch(element, source) == false) return false;
@@ -189,43 +189,44 @@ var devilmaycry555 = {
       path = path.split('.')
     }
     return function (element) {
-      for (i = 0; i < path.length; i++){
+      for (var i = 0; i < path.length; i++){
         element = element[path[i]]
       }
       return element
     }
   },
+  //??([{"user":"barney","active":false},{"user":"fred","active":false},{"user":"pebbles","active":true}],["active",false])
   findIndex: function (array, predicate, fromIndex = 0) {
     predicate = this.iteratee(predicate)
-    for (i = fromIndex; i < array.length; i++){
-      if (predicate(array[i]) == true) return i;
+    for (var i = fromIndex; i < array.length; i++){
+      if (predicate(array[i])) return i;
     }
     return -1
   },
   //O
   findLastIndex: function (array, predicate, fromIndex = array.length - 1) {
     predicate = this.iteratee(predicate)
-    for (i = fromIndex; i >= 0; i--){
-      if (predicate(array[i]) == true) return i;
+    for (var i = fromIndex; i >= 0; i--){
+      if (predicate(array[i])) return i;
     }
     return -1
   },
   //筛选语句可能是value，index/key，collection
-  //从fromindex开始，找到符合的即返回，找不到则返回undefined
+  //O从fromindex开始，找到符合的即返回，找不到则返回undefined
   find: function (collection, predicate, fromIndex = 0) {
-    for (i = fromIndex; i < collection.length; i++){
+    for (var i = fromIndex; i < collection.length; i++){
       if (predicate(collection[i]) == true) return collection[i];
     }
     return undefined
   },
   forEach: function (array, loop) {
-    for (i = 0; i < array.length; i++){
+    for (var i = 0; i < array.length; i++){
       loop(array[i])
     }
   },
   map: function (array, transform) {
     var mapped = []
-    for (i = 0; i < array.length; i++){
+    for (var i = 0; i < array.length; i++){
       mapped.push(transform(array[i]))
     }
     return mapped
@@ -233,14 +234,14 @@ var devilmaycry555 = {
   filter: function (array, test) {
     if (typeof test == 'function') {
       var passed = []
-      for (i = 0; i < array.length; i++){
+      for (var i = 0; i < array.length; i++){
         if (test(array[i])) passed.push(array[i]);
       }
       return passed
     }
     if (typeof test == 'object') {
       var passed = []
-      for (i = 0; i < array.length; i++){
+      for (var i = 0; i < array.length; i++){
         var allin = true
         for (var key in test) {
           if (!key in array[i] || test[key] !== array[i][key]) allin = false;
@@ -254,7 +255,7 @@ var devilmaycry555 = {
   reduce: function (ary_obj, combine, start = 0) {
     var current = start
     if (Array.isArray(ary_obj)) {
-      for (i = 0; i < ary_obj.length; i++){
+      for (var i = 0; i < ary_obj.length; i++){
         current = combine(current, ary_obj[i])
       }
     } else if (typeof ary_obj == 'object') {
@@ -314,7 +315,7 @@ var devilmaycry555 = {
     if (fromIndex < 0) {
       fromIndex += array.length
     }
-    for (i = fromIndex; i < array.length; i++){
+    for (var i = fromIndex; i < array.length; i++){
       if (array[i] == value) return i;
     }
     return -1
@@ -325,10 +326,10 @@ var devilmaycry555 = {
     return array
   },
   //求交集，根据第一个数组返回结果
-  //找不到就返回[]
+  //O找不到就返回[]
   intersection: function (arrays) {
     var a = arguments[0]
-    for (i = 1; i < arguments.length; i++){
+    for (var i = 1; i < arguments.length; i++){
       var f = arguments[i].filter(function (n) {
         for (var val of a) {
           if (n == val) {
@@ -346,7 +347,7 @@ var devilmaycry555 = {
   //O用所选符号将数组拼接成字符串
   join: function (array, separator = ',') {
     var a = array[0]
-    for (i = 1; i < array.length; i++){
+    for (var i = 1; i < array.length; i++){
       a += '' + separator + array[i]
     }
     return a
@@ -359,20 +360,80 @@ var devilmaycry555 = {
   },
   //O倒序找到所选的值的index
   lastIndexOf: function (array, value, fromIndex = array.length - 1) {
-    for (i = fromIndex; i >= 0; i--){
+    for (var i = fromIndex; i >= 0; i--){
       if (array[i] == value) return i;
     }
     return -1
   },
+  nth: function (array, n = 0) {
+    if (n >= 0) {
+      return array[n]
+    } else {
+      return array[array.length + n]
+    }
+  },
+  pull: function (array, values) {
+    for (var i = 1; i < arguments.length; i++){
+      this.remove(array, function (n) {
+        return n === arguments[i]
+      })
+    }
+    return array
+  },
+  pullAll: function (array, values) {
+    for (var i = 0; i < values.length; i++){
+      this.remove(array, function (n) {
+        return n === values[i]
+      })
+    }
+    return array
+  },
+  //将数组中符合条件的移入新数组并返回，且原数组不再含有这些项
+  remove: function (array, predicate) {
+    predicate = this.iteratee(predicate)
+    var a = array.slice()
+    var res = []
+    for (var i = 0; i < a.length; i++){
+      if (predicate(a[i])) res.push(array.splice(i, 1));
+    }
+    return res
+  },
   //O数组反转
   reverse: function (array) {
     var l = array.length - 1
-    for (i = 0; i*2 < l; i++){
+    for (var i = 0; i*2 < l; i++){
       var p = array[i]
       array[i] = array[l - i]
       array[l - i] = p
     }
     return array
+  },
+  //将value以最小index插入进有序数组中，返回index
+  //用二分法
+  sortedIndex: function (array, value) {
+    var left = 0
+    var right = array.length - 1
+    while (right - left > 1) {
+      var mid = Math.ceil(right + left)
+      if (array[mid] < value) {
+        left = mid
+      } else {
+        right = mid
+      }
+    }
+    return right
+  },
+  //以第一个数组为基本，一次跟后面数组比较，将自己没有的数push进来
+  union: function (arrays) {
+    var me = arguments[0]
+    var other = arguments.slice(1)
+    for (var n of other) {
+      me.concat(this.difference(me, n))
+    }
+    return me
+  },
+  uniqBy: function (arrays, iter) {
+    
   },
   //O去掉重复值
   uniq: function (array) {
@@ -403,7 +464,7 @@ var devilmaycry555 = {
   //O数组里面子数组相同index的值放入一个新数组，所有新数组组合成新的母项
   zip: function (...array) {
     var a = []
-    for (i = 0; i < array[0].length; i++){
+    for (var i = 0; i < array[0].length; i++){
       var m = []
       for (var s of array) {
         m.push(s[i])
@@ -522,7 +583,7 @@ var devilmaycry555 = {
   reject: function (array, test) {
     if (typeof test == 'function') {
       var passed = []
-      for (i = 0; i < array.length; i++){
+      for (var i = 0; i < array.length; i++){
         if (!test(array[i])) passed.push(array[i]);
       }
       return passed
